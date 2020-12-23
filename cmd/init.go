36fc0adba6f6f6d0836d2beb80759375875d9f30
Sahithyan <sahithyan2701@gmail.com>
2020-12-23 23:12:22 +0530

    save-locally flag added to init (not tested yet)

--- cmd/init.go
+++ cmd/init.go
@@ -3,10 +3,22 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/sahithyandev/honey-tree/helpers"
 	"github.com/sahithyandev/honey-tree/helpers/gitmanager"
+
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+)
+
+const (
+	_HONEY_TREE_CACHE_DIR = ".honey-tree"
+)
+
+var (
+	// TODO Test saveLocally feature
+	saveLocally bool
 )
 
 // initCmd represents the init command
@@ -22,6 +34,32 @@ var initCmd = &cobra.Command{
 			os.Exit(2)
 		}
 
+		var HOME_DIR, err = homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		fmt.Println("HOME", HOME_DIR)
+
+		var cacheLocation = path.Join(HOME_DIR, _HONEY_TREE_CACHE_DIR, boilerplateDirectory)
+
+		if saveLocally {
+			// TODO safely abort if boilerplateDirectory is local.
+			// cache it in the $HOME/.honey-tree/$BOILERPLATE_NAME
+			// if it already exists inside .honey-tree, delete it.
+			if helpers.DoesExist(cacheLocation) {
+				os.RemoveAll(cacheLocation)
+			}
+			gitmanager.CloneRepo(boilerplateDirectory, cacheLocation)
+		}
+
+		// check if boilerplate exists inside $HOME/.honey-tree/
+		// if it does, use it. show a warning.
+		if helpers.DoesExist(cacheLocation) {
+			fmt.Printf("%v is available locally (at %v). Local version will be used.\n", boilerplateDirectory, cacheLocation)
+			boilerplateDirectory = cacheLocation
+		}
+
 		gitmanager.CloneRepo(boilerplateDirectory, targetLocation)
 		gitmanager.ResetGitRepo(targetLocation)
 
@@ -30,6 +68,7 @@ var initCmd = &cobra.Command{
 }
 
 func init() {
+	initCmd.Flags().BoolVarP(&saveLocally, "save-locally", "s", false, "save the boilerplate locally (for future offline use)")
 
 	rootCmd.AddCommand(initCmd)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sahithyandev/honey-tree
 go 1.15
 
 require (
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 	gopkg.in/validator.v2 v2.0.0-20200605151824-2b28d334fa05
 )
